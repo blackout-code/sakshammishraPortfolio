211e7d512eb18528cd70a2f0e2fd817ebac60699
blackout-code <sakshamm97@gmail.com>
2026-06-19 19:35:38 +0530

    Setup GitHub Pages deployment with GitHub Actions

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  
   // Compression and optimization
   compress: true,
   productionBrowserSourceMaps: false,
